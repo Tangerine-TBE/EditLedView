07325a1ca66264fc573fb4380158a72aec0b221d
tangerine <1762094255@qq.com>
2023-11-13 22:58:08 +0800

    图片二进制黑白调整完成

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,31 +1,112 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSeekBar;
 
-import com.example.myapplication.Utils.PrefUtils;
+import com.example.myapplication.Utils.BitmapUtils;
+import com.example.myapplication.Utils.GlideEngine;
 import com.example.myapplication.weight.LedView;
+import com.jakewharton.rxbinding4.view.RxView;
+import com.luck.picture.lib.basic.PictureSelector;
+import com.luck.picture.lib.config.SelectMimeType;
+import com.luck.picture.lib.engine.UriToFileTransformEngine;
+import com.luck.picture.lib.entity.LocalMedia;
+import com.luck.picture.lib.interfaces.OnKeyValueResultCallbackListener;
+import com.luck.picture.lib.interfaces.OnResultCallbackListener;
+import com.luck.picture.lib.utils.SandboxTransformUtils;
+import com.tbruyelle.rxpermissions3.RxPermissions;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private LedView ledBig;
-    private boolean canTouch;
-    @SuppressLint({"MissingInflatedId", "ClickableViewAccessibility"})
+    private ImageView mOriginView;
+    private ImageView mCustomView;
+    private AppCompatSeekBar mThresholdController;
+    private Bitmap mOriginBitmap;
+
+    @SuppressLint({"MissingInflatedId", "ClickableViewAccessibility", "CheckResult"})
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ledBig = findViewById(R.id.led_view);
+        mOriginView = findViewById(R.id.iv_origin);
+        mCustomView = findViewById(R.id.iv_effect);
+        mThresholdController = findViewById(R.id.sb_controller);
+        mThresholdController.setMax(255);
+        mThresholdController.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (mOriginBitmap != null) {
+                    Bitmap mEffectBitmap = BitmapUtils.convertToBlackAndWhite(mOriginBitmap, i);
+                    mCustomView.setImageBitmap(mEffectBitmap);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        RxPermissions rxPermissions = new RxPermissions(this);
+        RxView.clicks(findViewById(R.id.btn_canTouch)).compose(rxPermissions.ensure(Manifest.permission.READ_EXTERNAL_STORAGE))
+                        .subscribe(granted ->{
+                            if(granted){
+                                PictureSelector.create(this).openGallery(SelectMimeType.ofImage()).setSandboxFileEngine(new UriToFileTransformEngine() {
+                                    @Override
+                                    public void onUriToFileAsyncTransform(Context context, String srcPath, String mineType, OnKeyValueResultCallbackListener call) {
+                                        if (call != null) {
+                                            String sandboxPath = SandboxTransformUtils.copyPathToSandbox(context, srcPath, mineType);
+                                            call.onCallback(srcPath,sandboxPath);
+                                        }
+                                    }
+                                }).setImageEngine(GlideEngine.createGlideEngine()).setMaxSelectNum(1).forResult(new OnResultCallbackListener<LocalMedia>() {
+                                    @Override
+                                    public void onResult(ArrayList<LocalMedia> result) {
+                                        LocalMedia localMedia = result.get(0);
+                                        if (localMedia != null) {
+                                            String filePath = localMedia.getSandboxPath();
+                                            Bitmap originBitmap = BitmapFactory.decodeFile(filePath);
+                                            mOriginView.setImageBitmap(originBitmap);
+                                            mOriginBitmap = originBitmap;
+                                            mCustomView.setImageBitmap(BitmapUtils.convertToBlackAndWhite(originBitmap, 0));
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancel() {
+
+                                    }
+                                });
+                            }
+                        });
         findViewById(R.id.btn_reverser).setOnClickListener(view -> ledBig.reverse());
         findViewById(R.id.btn_clear).setOnClickListener(view -> ledBig.clear());
-        findViewById(R.id.btn_canTouch).setOnClickListener(view->{
-                //Todo 选择一张图片
-        });
         findViewById(R.id.btn_save).setOnClickListener(view -> {
-                //Todo 保存一张图片
+            //Todo 保存一张图片
         });
         this.ledBig.setIsCanTouch(true);
         int matrix = 11;
--- app/src/main/java/com/example/myapplication/Utils/BitmapUtils.java
+++ app/src/main/java/com/example/myapplication/Utils/BitmapUtils.java
@@ -1,40 +1,36 @@
 package com.example.myapplication.Utils;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.media.ThumbnailUtils;
 
 public class BitmapUtils {
 
-    public static Bitmap convertToBMW(Bitmap paramBitmap, int paramInt1, int paramInt2, int paramInt3) {
-        int i = paramBitmap.getWidth();
-        int j = paramBitmap.getHeight();
-        int[] arrayOfInt = new int[i * j];
-        paramBitmap.getPixels(arrayOfInt, 0, i, 0, 0, i, j);
-        for (byte b = 0; b < j; b++) {
-            for (byte b1 = 0; b1 < i; b1++) {
-                byte b2;
-                boolean bool;
-                int k = i * b + b1;
-                int m = arrayOfInt[k];
-                char c = 'y';
-                if ((0xFF0000 & m) >> 16 > paramInt3) {
-                    b2 = (byte) 255;
-                } else {
-                    b2 = 0;
-                }
-                bool = (m & 0xFF) > paramInt3;
-                if ((0xFF00 & m) >> 8 <= paramInt3)
-                    c = Character.MIN_VALUE;
-                arrayOfInt[k] = (bool ? 0xFF000000 : 0) | (b2 << 16) | (c << 8) | (m & 0xFF);
-                if (arrayOfInt[k] == -1) {
-                    arrayOfInt[k] = -1;
-                } else {
-                    arrayOfInt[k] = -16777216;
-                }
+    public static Bitmap convertToBlackAndWhite(Bitmap bitmap, int threshold) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        Bitmap blackAndWhiteBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        int[] pixels = new int[width * height];
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+
+        for (int i = 0; i < pixels.length; i++) {
+            int pixel = pixels[i];
+            int red = Color.red(pixel);
+            int green = Color.green(pixel);
+            int blue = Color.blue(pixel);
+
+            int average = (red + green + blue) / 3;
+
+            if (average > threshold) {
+                pixels[i] = Color.WHITE;
+            } else {
+                pixels[i] = Color.BLACK;
             }
         }
-        paramBitmap = Bitmap.createBitmap(i, j, Bitmap.Config.ARGB_8888);
-        paramBitmap.setPixels(arrayOfInt, 0, i, 0, 0, i, j);
-        return ThumbnailUtils.extractThumbnail(paramBitmap, paramInt1, paramInt2);
+
+        blackAndWhiteBitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+
+        return blackAndWhiteBitmap;
     }
 }
