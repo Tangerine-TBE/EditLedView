56101fbb7e6e2d2f001787ae84cf0f137fe6af59
tangerine <1762094255@qq.com>
2023-11-15 20:06:33 +0800

    收工

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -119,20 +119,5 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_save).setOnClickListener(view -> {
             //Todo 保存一张图片
         });
-        this.ledBig.setIsCanTouch(true);
-        int matrix = 11;
-        byte b = 0;
-        int i;
-        StringBuilder stringBuilder = new StringBuilder();
-        while (true) {
-            i = matrix;
-            if (b < i * i) {
-                stringBuilder.append("0");
-                b++;
-                continue;
-            }
-//            this.ledBig.setLEDData(stringBuilder.toString());
-            return;
-        }
     }
 }
